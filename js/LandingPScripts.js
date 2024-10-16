9f23eb8c39b6f186b7eb104ce8686701049f43f9
Sebastian Chacon B <87223451+SebastianCHB@users.noreply.github.com>
2024-10-15 23:20:45 -0600

    Add files via upload

---
+++ js/LandingPScripts.js
@@ -0,0 +1,12 @@
+/*Cambio A Login */
+function changeLogin(){
+    window.location.href='login.html'
+}
+/*Cambio a Signup*/
+function changeSignup(){
+    window.location.href='signup.html'
+}
+/*Cambio a landing page*/
+function changeLandingp(){
+    window.location.href='index.html'
+}
