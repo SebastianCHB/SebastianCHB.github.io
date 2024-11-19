1e64a2f4c008d2417d60978904cf63fa94c93642
Sebastian Chacon B <87223451+SebastianCHB@users.noreply.github.com>
2024-11-19 01:35:21 -0600

    Update payment.js

--- PDA/js/payment.js
+++ PDA/js/payment.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.btndeletepay').forEach(button => {
     button.onclick = (evt) => {
       Swal.fire({
-        title: "Do you want to delete this payment method",
+        title: "Do you want to delete this payment method?",
         icon: "warning",
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
